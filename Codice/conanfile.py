f23f0cf61f79f07fcd2e833d9a74267575584a99
Dario Passet <dario.passet@gmail.com>
2026-03-11 20:02:59 +0000

    Installazione automatica dei pacchetti di sistema tramite Conan e configurata la libreria SDL3 togliendo i componenti non necessari.

--- Codice/conanfile.py
+++ Codice/conanfile.py
@@ -1,5 +1,6 @@
 ﻿from conan import ConanFile
 from conan.tools.cmake import cmake_layout, CMakeDeps
+from conan.tools.system.package_manager import Apt
 
 class ConanApplication(ConanFile):
     package_type = "application"
@@ -11,14 +12,32 @@ class ConanApplication(ConanFile):
 
     def generate(self):
         cmake = CMakeDeps(self)
-        cmake.generate();
+        cmake.generate()
+
+    def system_requirements(self):
+        packages = ["libasound2-dev", "libvulkan-dev", "vulkan-utility-libraries-dev"]
+        apt = Apt(self)
+        result = apt.install(packages)
+        if result is not None and result != 0:
+            apt.install(packages, update=True)
 
     def requirements(self):
         self.requires("boost/1.88.0")
         # 'force' serve a far si che PlutoSVG usi questa versione invece della 2.13.2
         self.requires("freetype/2.14.1", force=True)
-        self.requires("libalsa/1.2.13") # La stessa versione usata dalla libreria SDL v3.2.20
         self.requires("nlohmann_json/3.12.0")
         self.requires("platformfolders/4.3.0")
         self.requires("plutosvg/0.0.7")
         self.requires("sdl/3.2.20")
+        #self.requires("libalsa/system", force=True)
+
+    def configure(self):
+        sdl = self.options["sdl/*"]
+        sdl.shared = False
+        sdl.audio = False
+        sdl.render = False
+        sdl.Camera = False
+        sdl.opengl = False
+        sdl.opengles = False
+        sdl.metal = False
+        sdl.directx = False
